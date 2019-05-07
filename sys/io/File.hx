97efc884e911f221cfeecc9d058853ceb96290f8
Aurel Bílý <aurel.bily@gmail.com>
2019-05-07 17:15:33 +0100

    make sys.io.File sync, add AsyncFile

--- sys/io/File.hx
+++ sys/io/File.hx
@@ -18,22 +18,23 @@ extern class File {
   static inline function update(path:String, ?binary:Bool = true):FileOutput return sys.FileSystem.open(path, ReadWrite).output;
   static inline function write(path:String, ?binary:Bool = true):FileOutput return sys.FileSystem.open(path, WriteTruncate).output;
   
-  public var output(default, null):Null<FileOutput>;
-  public var input(default, null):Null<FileInput>;
+  final output:Null<FileOutput>;
+  final input:Null<FileInput>;
+  final async:AsyncFile;
   
   // node compatibility (file descriptors)
-  function appendFile(data:Bytes, ?flags:FileOpenFlags, ?mode:FileMode, callback:(?error:Error)->Void):Void;
-  function chmod(mode:FileMode, callback:(?error:Error)->Void):Void;
-  function chown(uid:Int, gid:Int, callback:(?error:Error)->Void):Void;
-  function close(callback:(?error:Error)->Void):Void;
-  function datasync(callback:(?error:Error)->Void):Void;
-  function read(buffer:Bytes, offset:Int, length:Int, position:Int, callback:(?error:Error, ?bytesRead:Int, ?buffer:Bytes)->Void):Void;
-  function readFile(?flags:FileOpenFlags, callback:(?error:Error, ?bytes:Bytes)->Void):Void;
-  function stat(callback:(?error:Error, ?stats:FileStat)->Void):Void;
-  function sync(callback:(?error:Error)->Void):Void;
-  function truncate(?len:Int = 0, callback:(?error:Error)->Void):Void;
-  function utimes(atime:Date, mtime:Date, callback:(?error:Error)->Void):Void;
-  function write(buffer:Bytes, offset:Int, length:Int, position:Int, callback:(?error:Error, ?bytesWritten:Int, ?buffer:Bytes)->Void):Void;
-  function writeString(str:String, ?position:Int, ?encoding:Encoding, callback:(?error:Error, ?bytesRead:Int, ?buffer:Bytes)->Void):Void;
-  function writeFile(data:Bytes, ?flags:FileOpenFlags, ?mode:FileMode, callback:(?error:Error)->Void):Void;
+  function appendFile(data:Bytes, ?flags:FileOpenFlags, ?mode:FileMode):Void;
+  function chmod(mode:FileMode):Void;
+  function chown(uid:Int, gid:Int):Void;
+  function close():Void;
+  function datasync():Void;
+  function read(buffer:Bytes, offset:Int, length:Int, position:Int):{bytesRead:Int, buffer:Bytes};
+  function readFile(?flags:FileOpenFlags):Bytes;
+  function stat():FileStat;
+  function sync():Void;
+  function truncate(?len:Int = 0):Void;
+  function utimes(atime:Date, mtime:Date):Void;
+  function write(buffer:Bytes, offset:Int, length:Int, position:Int):{bytesWritten:Int, buffer:Bytes};
+  function writeString(str:String, ?position:Int, ?encoding:Encoding):{bytesWritten:Int, buffer:Bytes};
+  function writeFile(data:Bytes, ?flags:FileOpenFlags, ?mode:FileMode):Void;
 }
