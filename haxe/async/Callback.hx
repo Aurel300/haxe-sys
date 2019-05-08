1425c47bcd867e6d5fb6154b5b910f71a6ba5e85
Aurel Bílý <aurel.bily@gmail.com>
2019-05-08 14:44:43 +0100

    Callback froms

--- haxe/async/Callback.hx
+++ haxe/async/Callback.hx
@@ -6,6 +6,7 @@ typedef CallbackData<T> = (?error:Error, ?result:T)->Void;
 
 @:callable
 abstract Callback<T>(CallbackData<T>) from CallbackData<T> {
-  @:from static inline function fromErrorOnly<T>(f:(?error:Error)->Void):Callback<T> return (?err, ?result) -> f(err);
-  @:from static inline function fromResultOnly<T>(f:(?result:T)->Void):Callback<T> return (?err, ?result) -> f(result);
+  @:from static inline function fromOptionalErrorOnly(f:(?error:Error)->Void):Callback<NoData> return (?err:Error, ?result:NoData) -> f(err);
+  @:from static inline function fromErrorOnly(f:(error:Error)->Void):Callback<NoData> return (?err:Error, ?result:NoData) -> f(err);
+  @:from static inline function fromResultOnly<T>(f:(?result:T)->Void):Callback<T> return (?err:Error, ?result:T) -> f(result);
 }
