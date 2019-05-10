89725ed1b011df5fa3bd7ab7b3456706c90efa6d
Aurel Bílý <aurel.bily@gmail.com>
2019-05-10 19:22:30 +0100

    URL

---
+++ sys/net/Url.hx
@@ -0,0 +1,53 @@
+package sys.net;
+
+extern class Url { // once implemented might be abstract over String
+  static function domainToASCII(domain:String):String;
+  static function domainToUnicode(domain:String):String;
+  static function fileURLToPath(url:String):String;
+  static function format(url:Url, ?opt:UrlFormatOptions):String;
+  static function pathToFileURL(path:String):Url;
+  
+  var hash:String;
+  var host:String; // = hostname + port
+  var hostname:String;
+  var href:String;
+  var origin:String;
+  var password:String;
+  var pathname:String;
+  var port:Int;
+  var protocol:String;
+  var search:String;
+  var searchParams:UrlSearchParams;
+  var username:String;
+  
+  function new(input:String, ?base:String);
+  function toString():String;
+  function toJSON():String;
+}
+
+typedef UrlFormatOptions = {
+     ?auth:Bool
+    ,?fragment:Bool
+    ,?search:Bool
+    ,?unicode:Bool
+  };
+
+extern class UrlSearchParams {
+  static function fromString(string:String):UrlSearchParams;
+  static function fromMap(map:Map<String, Array<String>>):UrlSearchParams;
+  static function fromIterable(iterable:KeyValueIterable<String, String>):UrlSearchParams;
+  
+  function new();
+  function append(name:String, value:String):Void;
+  function delete(name:String):Void;
+  // function entries():KeyValueIterator<String, String>;
+  // function forEach();
+  function get(name:String):Null<String>;
+  function getAll(name:String):Array<String>;
+  function has(name:String):Bool;
+  // function keys();
+  function set(name:String, value:String):Void;
+  // function sort();
+  function toString():String;
+  // function values();
+}
